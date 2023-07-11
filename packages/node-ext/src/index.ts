88831606c45cd4f3ff9e07560a25f5e2ebd37aeb
joshunrau <contact@joshuaunrau.com>
2023-07-11 12:02:44 -0400

    remove flawed __dirname

--- packages/node-ext/src/index.ts
+++ packages/node-ext/src/index.ts
@@ -1,9 +1,4 @@
 import fs from 'node:fs';
-import path from 'node:path';
-import url from 'node:url';
-
-export const __filename = url.fileURLToPath(import.meta.url);
-export const __dirname = path.dirname(__filename);
 
 export function readJSON<T = unknown>(filepath: string) {
   const content = fs.readFileSync(filepath, { encoding: 'utf-8' });
