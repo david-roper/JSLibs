bb4a2ebf7e30a404032a3911e893b387fa227afe
joshunrau <contact@joshuaunrau.com>
2023-09-21 14:26:13 -0400

    setup ajv for bun

---
+++ packages/ajv/src/index.ts
@@ -0,0 +1,14 @@
+import Ajv from 'ajv';
+import addErrors from 'ajv-errors';
+import addFormats from 'ajv-formats';
+
+
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class AjvFactory {
+  static create() {
+    const ajv = new Ajv({ allErrors: true, coerceTypes: false, strict: true });
+    addErrors(ajv);
+    addFormats(ajv);
+    return ajv;
+  }
+}
