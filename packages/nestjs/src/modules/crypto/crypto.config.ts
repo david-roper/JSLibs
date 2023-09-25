d99a68320125441ab511eb8d6d4c2898818150be
joshunrau <contact@joshuaunrau.com>
2023-09-25 14:18:31 -0400

    add global option

--- packages/nestjs/src/modules/crypto/crypto.config.ts
+++ packages/nestjs/src/modules/crypto/crypto.config.ts
@@ -5,6 +5,12 @@ interface CryptoModuleOptions {
 }
 
 const { ConfigurableModuleClass: ConfigurableCryptoModule, MODULE_OPTIONS_TOKEN: CRYPTO_MODULE_OPTIONS_TOKEN } =
-  new ConfigurableModuleBuilder<CryptoModuleOptions>().build();
+  new ConfigurableModuleBuilder<CryptoModuleOptions>()
+    .setClassMethodName('forRoot')
+    .setExtras({ isGlobal: false }, (definition, extras) => ({
+      ...definition,
+      global: extras.isGlobal
+    }))
+    .build();
 
 export { type CryptoModuleOptions, ConfigurableCryptoModule, CRYPTO_MODULE_OPTIONS_TOKEN };
