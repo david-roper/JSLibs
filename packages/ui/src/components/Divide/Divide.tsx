97adefafc6b2e2fd3ea69acd678232a59bf4b790
joshunrau <contact@joshuaunrau.com>
2023-10-02 16:29:34 -0400

    adjust divide color

--- packages/ui/src/components/Divide/Divide.tsx
+++ packages/ui/src/components/Divide/Divide.tsx
@@ -1,5 +1,5 @@
 import { twMerge } from 'tailwind-merge';
 
 export const Divide = ({ className, ...props }: React.ComponentPropsWithoutRef<'hr'>) => (
-  <hr className={twMerge('mt-auto h-[1px] border-none bg-slate-700', className)} {...props} />
+  <hr className={twMerge('mt-auto h-[1px] border-none bg-slate-300 dark:bg-slate-700', className)} {...props} />
 );
