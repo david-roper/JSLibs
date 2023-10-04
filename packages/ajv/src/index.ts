c9475ec132fd1824a2908be01e790d949621a8c8
joshunrau <contact@joshuaunrau.com>
2023-10-04 10:19:19 -0400

    add ts expect err

--- packages/ajv/src/index.ts
+++ packages/ajv/src/index.ts
@@ -5,8 +5,9 @@ import addFormats from 'ajv-formats';
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class AjvFactory {
   static create() {
-    const ajv = new Ajv({ allErrors: true, coerceTypes: false, strict: true });
+    const ajv = new Ajv({ allErrors: true, coerceTypes: false });
     addErrors(ajv);
+    // @ts-expect-error
     addFormats(ajv);
     return ajv;
   }
