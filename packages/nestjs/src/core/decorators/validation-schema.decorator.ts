0ed6cd1a87bfdf9b585d09ba59c8ec4767568505
joshunrau <contact@joshuaunrau.com>
2023-10-13 18:00:20 -0400

    update comment

--- packages/nestjs/src/core/decorators/validation-schema.decorator.ts
+++ packages/nestjs/src/core/decorators/validation-schema.decorator.ts
@@ -2,9 +2,7 @@ import type { Class } from 'type-fest';
 import type { ZodType } from 'zod';
 
 /**
- * Decorator to define the validation schema for DTO objects. Modifies the default
- * AJV behavior such that if not defined, additional properties are prohibited.
- */
+ * Decorator to define the zod validation schema for DTO classes */
 export function ValidationSchema<T, U extends ZodType<T>>(schema: U) {
   return (target: Class<T>) => {
     Reflect.defineMetadata('ValidationSchema', schema, target);
