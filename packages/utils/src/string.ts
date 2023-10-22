f3c3f1c5e1f606212ba7e8352d20969a1f8b0daf
joshunrau <contact@joshuaunrau.com>
2023-10-21 21:43:56 -0400

    @douglasneuroinformatics/utils@1.0.3

--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -1,7 +1,12 @@
-export function camelToSnakeCase(s: string) {
-  return s.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
+import type { CamelCase, SnakeCase } from 'type-fest';
+
+export function camelToSnakeCase<T extends string>(s: T) {
+  return s.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`) as SnakeCase<T>;
 }
 
-export function snakeToCamelCase(s: string) {
-  return s.toLowerCase().replace(/([-_][a-z])/g, (group) => group.toUpperCase().replace('-', '').replace('_', ''));
+export function snakeToCamelCase<T extends string>(s: T) {
+  return s
+    .toLowerCase()
+    .replace(/([-_][a-z])/g, (group) => group.toUpperCase().replace('-', '').replace('_', '')) as CamelCase<T>;
 }
+
