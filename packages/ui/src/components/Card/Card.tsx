a1082d3c51017d9da1dc562f85ea0fd30be7e5ec
joshunrau <contact@joshuaunrau.com>
2023-10-24 13:12:43 -0400

    add forward ref to card

--- packages/ui/src/components/Card/Card.tsx
+++ packages/ui/src/components/Card/Card.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 
 import { twMerge } from 'tailwind-merge';
 
-export const Card = ({ children, className, ...props }: React.HTMLProps<HTMLDivElement>) => (
-  <div
-    className={twMerge(
-      'bg-slate-50 dark:bg-slate-800 shadow-md rounded-md ring-1 ring-slate-900/5 dark:ring-slate-100/20',
-      className
-    )}
-    {...props}
-  >
-    {children}
-  </div>
-);
+export const Card = React.forwardRef<HTMLDivElement, React.HTMLProps<HTMLDivElement>>(function Card(
+  { children, className, ...props },
+  ref
+) {
+  return (
+    <div
+      className={twMerge(
+        'bg-slate-50 dark:bg-slate-800 shadow-md rounded-md ring-1 ring-slate-900/5 dark:ring-slate-100/20',
+        className
+      )}
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+});
