159c8ef61d7f0606788450fd2e5246807bc42a91
joshunrau <contact@joshuaunrau.com>
2023-10-27 11:37:54 -0400

    extract app request

--- packages/nestjs/src/core/decorators/current-user.decorator.ts
+++ packages/nestjs/src/core/decorators/current-user.decorator.ts
@@ -1,12 +1,6 @@
 import { type ExecutionContext, createParamDecorator } from '@nestjs/common';
-import { type Request } from 'express';
 
-// This should be augmented in the implementation
-// See https://stackoverflow.com/questions/66312048/cant-override-express-request-user-type-but-i-can-add-new-properties-to-reques
-type AppRequest = Request & {
-  user: object;
-};
-
+import type { AppRequest } from '../types';
 /**
  * Extract the user from the request object
  * @param key - the key of the user object to extract, or omit for the entire user
--- packages/nestjs/src/core/types.ts
+++ packages/nestjs/src/core/types.ts
@@ -1,4 +1,11 @@
+import { type Request } from 'express';
 import type { Class } from 'type-fest';
+
+// This should be augmented in the implementation
+// See https://stackoverflow.com/questions/66312048/cant-override-express-request-user-type-but-i-can-add-new-properties-to-reques
+export type AppRequest = Request & {
+  user: object;
+};
 
 export type EntityClass<T extends object> = Class<T> & {
   readonly modelName: string;
