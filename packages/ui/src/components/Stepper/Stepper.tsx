f3d1f67edf92ebd49e94123412730aa4275f1b18
joshunrau <contact@joshuaunrau.com>
2023-11-03 12:32:33 -0400

    fix logic error in stepper

--- packages/ui/src/components/Stepper/Stepper.tsx
+++ packages/ui/src/components/Stepper/Stepper.tsx
@@ -2,9 +2,9 @@
 
 import React, { useReducer } from 'react';
 
+import clsx from 'clsx';
+
 import { StepperContext } from '../../context/StepperContext';
-import { StepperDivider } from './StepperDivider';
-import { StepperIcon } from './StepperIcon';
 
 type Step = {
   element: React.ReactElement;
@@ -29,12 +29,36 @@ const Stepper = ({ steps }: StepperProps) => {
   return (
     <StepperContext.Provider value={{ index, updateIndex }}>
       <div className="mb-16 flex items-center print:hidden">
-        {steps.map((step, i) => (
-          <React.Fragment key={i}>
-            <StepperIcon icon={step.icon} isActive={i === index} label={step.label} />
-            {i < steps.length - 1 && <StepperDivider isActive={i < index} />}
-          </React.Fragment>
-        ))}
+        {steps.map((step, i) => {
+          return (
+            <React.Fragment key={i}>
+              <div>
+                <div className="relative flex items-center">
+                  <div
+                    className={clsx(
+                      'h-12 w-12 rounded-full text-white bg-sky-700 border-2 border-sky-700 py-3 transition duration-500 ease-in-out [&>*]:h-full [&>*]:w-full',
+                      {
+                        'opacity-25': i > index
+                      }
+                    )}
+                  >
+                    {step.icon}
+                  </div>
+                  <div className="absolute top-0 -ml-10 mt-16 w-32 text-center text-xs font-medium uppercase text-slate-600 dark:text-slate-300">
+                    {step.label}
+                  </div>
+                </div>
+              </div>
+              {i !== steps.length - 1 && (
+                <div
+                  className={clsx('flex-auto border-sky-700 border-t-2 transition duration-500 ease-in-out', {
+                    'border-opacity-25': i >= index
+                  })}
+                />
+              )}
+            </React.Fragment>
+          );
+        })}
       </div>
       <div>{steps[index]?.element}</div>
     </StepperContext.Provider>
