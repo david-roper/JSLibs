0f1a39d7edaefc3aef8351d4650247752fbc7567
joshunrau <contact@joshuaunrau.com>
2023-11-29 18:01:36 -0500

    add more methods

--- packages/nestjs/src/modules/database/database.repository.ts
+++ packages/nestjs/src/modules/database/database.repository.ts
@@ -1,6 +1,16 @@
-import { InternalServerErrorException } from '@nestjs/common';
+/* eslint-disable perfectionist/sort-classes */
+import { InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { ObjectId } from 'mongodb';
-import type { Collection, Db, Document, Filter, FindOptions, OptionalUnlessRequiredId } from 'mongodb';
+import type {
+  Collection,
+  Db,
+  DeleteOptions,
+  Document,
+  Filter,
+  FindOptions,
+  OptionalUnlessRequiredId,
+  UpdateOptions
+} from 'mongodb';
 
 import { InjectDatabaseConnection } from './database.decorators';
 
@@ -31,19 +41,49 @@ export function DatabaseRepository<T extends Document>(entity: EntityClass<T>) {
       return this.collection.findOne(filter, options).then((value) => value !== null);
     }
 
-    find(filter: Filter<T>, options: FindOptions = {}) {
+    find(filter: Filter<T> = {}, options: FindOptions = {}) {
       return this.collection.find(filter, options).toArray();
     }
 
     findById(id: ObjectIdLike, options: FindOptions = {}) {
-      if (!ObjectId.isValid(id)) {
-        throw new InternalServerErrorException(`Cannot coerce value to ObjectID: ${id.toString()}`);
-      }
+      this.checkId(id);
       return this.collection.findOne({ _id: new ObjectId(id) } as Filter<T>, options);
     }
 
     findOne(filter: Filter<T>, options: FindOptions = {}) {
       return this.collection.findOne(filter, options);
+    }
+
+    async updateOne(filter: Filter<T>, update: Partial<T>, options: UpdateOptions = {}) {
+      const result = await this.collection.updateOne(filter, update, options);
+      if (!(result.acknowledged && result.upsertedId)) {
+        throw new NotFoundException('Failed to update entity: not found!');
+      }
+      return (await this.findById(result.upsertedId))!;
+    }
+
+    async updateById(id: ObjectIdLike, update: Partial<T>, options: UpdateOptions = {}) {
+      this.checkId(id);
+      return this.updateOne({ _id: new ObjectId(id) } as Filter<T>, update, options);
+    }
+
+    async deleteOne(filter: Filter<T>, options: DeleteOptions = {}) {
+      const result = await this.collection.deleteOne(filter, options);
+      if (!(result.acknowledged && result.deletedCount)) {
+        throw new NotFoundException('Failed to delete entity: not found!');
+      }
+      return { success: true };
+    }
+
+    async deleteById(id: ObjectIdLike, options: DeleteOptions = {}) {
+      this.checkId(id);
+      return this.deleteOne({ _id: new ObjectId(id) } as Filter<T>, options);
+    }
+
+    private checkId(id: ObjectIdLike) {
+      if (!ObjectId.isValid(id)) {
+        throw new InternalServerErrorException(`Cannot coerce value to ObjectID: ${id.toString()}`);
+      }
     }
   }
   return Repository;
