e37dd26f188c18805174af802eb73f69c218208e
joshunrau <contact@joshuaunrau.com>
2024-01-15 11:02:24 -0500

    less verbose in dev

--- packages/nestjs/src/modules/logging/logging.module.ts
+++ packages/nestjs/src/modules/logging/logging.module.ts
@@ -1,5 +1,9 @@
 import { type DynamicModule, Module } from '@nestjs/common';
+import type { Request, Response } from 'express';
 import { LoggerModule } from 'nestjs-pino';
+import type { DestinationStream } from 'pino';
+import type { Options as PinoHttpOptions } from 'pino-http';
+import PinoPretty from 'pino-pretty';
 
 @Module({})
 export class LoggingModule {
@@ -7,12 +11,32 @@ export class LoggingModule {
     return {
       imports: [
         LoggerModule.forRoot({
-          pinoHttp: {
-            transport: process.env.NODE_ENV !== 'production' ? { target: 'pino-pretty' } : undefined
-          }
+          pinoHttp: this.getPinoHttpOptions()
         })
       ],
       module: LoggingModule
     };
+  }
+
+  private static getPinoHttpOptions(): [PinoHttpOptions, DestinationStream] | undefined {
+    if (process.env.NODE_ENV === 'production') {
+      return;
+    }
+    return [
+      {
+        serializers: {
+          req: (req: Request) => {
+            return `${req.method} ${req.url}`;
+          },
+          res: (res: Response) => {
+            return res.statusCode;
+          }
+        },
+        wrapSerializers: false
+      },
+      PinoPretty({
+        colorize: true
+      })
+    ];
   }
 }
