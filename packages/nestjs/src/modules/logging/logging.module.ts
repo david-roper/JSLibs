a1c103d7fec20e3c1c1c57bbdc0813fbf13bba69
joshunrau <contact@joshuaunrau.com>
2024-01-15 11:11:32 -0500

    add stack trace

--- packages/nestjs/src/modules/logging/logging.module.ts
+++ packages/nestjs/src/modules/logging/logging.module.ts
@@ -1,6 +1,8 @@
 import { type DynamicModule, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import type { Request, Response } from 'express';
 import { LoggerModule } from 'nestjs-pino';
+import { LoggerErrorInterceptor } from 'nestjs-pino';
 import type { DestinationStream } from 'pino';
 import type { Options as PinoHttpOptions } from 'pino-http';
 import PinoPretty from 'pino-pretty';
@@ -9,12 +11,14 @@ import PinoPretty from 'pino-pretty';
 export class LoggingModule {
   static forRoot(): DynamicModule {
     return {
+      global: true,
       imports: [
         LoggerModule.forRoot({
           pinoHttp: this.getPinoHttpOptions()
         })
       ],
-      module: LoggingModule
+      module: LoggingModule,
+      providers: this.getProviders()
     };
   }
 
@@ -37,6 +41,18 @@ export class LoggingModule {
       PinoPretty({
         colorize: true
       })
+    ];
+  }
+
+  private static getProviders() {
+    if (process.env.NODE_ENV === 'production') {
+      return;
+    }
+    return [
+      {
+        provide: APP_INTERCEPTOR,
+        useClass: LoggerErrorInterceptor
+      }
     ];
   }
 }
