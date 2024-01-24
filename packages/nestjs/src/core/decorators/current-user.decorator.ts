b8c5449522f15efda38583f61fe6a2b47579109e
joshunrau <contact@joshuaunrau.com>
2024-01-24 09:53:01 -0500

    fix issue where type of param decorator is incorrectly removed by tsc

--- packages/nestjs/src/core/decorators/current-user.decorator.ts
+++ packages/nestjs/src/core/decorators/current-user.decorator.ts
@@ -1,11 +1,18 @@
-import { type ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { createParamDecorator } from '@nestjs/common';
+import type { ExecutionContext } from '@nestjs/common';
 
 import type { AppRequest } from '../types';
+
+type CurrentUserDecorator = (key: Extract<keyof AppRequest['user'], string>) => ParameterDecorator;
+
 /**
  * Extract the user from the request object
  * @param key - the key of the user object to extract, or omit for the entire user
  */
-export const CurrentUser = createParamDecorator<keyof AppRequest['user'], ExecutionContext>((key, context) => {
+export const CurrentUser: CurrentUserDecorator = createParamDecorator<
+  Extract<keyof AppRequest['user'], string>,
+  ExecutionContext
+>((key, context) => {
   const request = context.switchToHttp().getRequest<AppRequest>();
   if (key) {
     return request.user?.[key];
--- packages/nestjs/src/core/types.ts
+++ packages/nestjs/src/core/types.ts
@@ -1,7 +1,7 @@
-import { type Request } from 'express';
+import type { Request } from 'express';
 
 // This should be augmented in the implementation
 // See https://stackoverflow.com/questions/66312048/cant-override-express-request-user-type-but-i-can-add-new-properties-to-reques
 export type AppRequest = Request & {
-  user: object;
+  user: Record<string, unknown>;
 };
