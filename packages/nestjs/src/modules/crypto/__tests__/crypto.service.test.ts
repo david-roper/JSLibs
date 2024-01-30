284b2eccf0b129641f8b500fc6d82e81cb66921d
joshunrau <contact@joshuaunrau.com>
2024-01-30 13:11:32 -0500

    remove refactored code from CryptoService

--- packages/nestjs/src/modules/crypto/__tests__/crypto.service.test.ts
+++ packages/nestjs/src/modules/crypto/__tests__/crypto.service.test.ts
@@ -4,7 +4,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { CRYPTO_MODULE_OPTIONS_TOKEN, type CryptoModuleOptions } from '../crypto.config';
 import { CryptoService } from '../crypto.service';
-import { AsymmetricEncryptionKeyPair, SerializableUint8Array } from '../crypto.utils';
 
 describe('CryptoService', () => {
   let cryptoService: CryptoService;
@@ -54,69 +53,6 @@ describe('CryptoService', () => {
     it('should return false when comparing a hash with an incorrect value', async () => {
       const hash = await cryptoService.hashPassword('foo');
       expect(cryptoService.comparePassword('bar', hash)).resolves.toBeFalse();
-    });
-  });
-
-  describe('generateKeyPair', () => {
-    let keyPair: AsymmetricEncryptionKeyPair;
-
-    beforeEach(async () => {
-      keyPair = await cryptoService.generateKeyPair();
-    });
-
-    it('should generate a key pair with private and public keys', () => {
-      expect(keyPair).toHaveProperty('privateKey');
-      expect(keyPair).toHaveProperty('publicKey');
-    });
-    it('should have private and public keys that are instances of CryptoKey', () => {
-      expect(keyPair.privateKey).toBeInstanceOf(CryptoKey);
-      expect(keyPair.publicKey).toBeInstanceOf(CryptoKey);
-    });
-    it('should return an instance of AsymmetricEncryptionKeyPair', () => {
-      expect(keyPair).toBeInstanceOf(AsymmetricEncryptionKeyPair);
-    });
-  });
-
-  describe('encrypt and decrypt', () => {
-    const originalText = 'Cela devrait fonctionner avec les caractères unicode 😃';
-    let keyPair: AsymmetricEncryptionKeyPair;
-
-    beforeEach(async () => {
-      keyPair = await cryptoService.generateKeyPair();
-    });
-
-    it('encrypt should return an instance of SerializableUint8Array', async () => {
-      const encrypted = await cryptoService.encrypt(originalText, keyPair.publicKey);
-      expect(encrypted).toBeInstanceOf(SerializableUint8Array);
-    });
-
-    it('decrypt should return original text', async () => {
-      const encrypted = await cryptoService.encrypt(originalText, keyPair.publicKey);
-      const decrypted = await cryptoService.decrypt(encrypted, keyPair.privateKey);
-      expect(decrypted).toBe(originalText);
-    });
-
-    it('decrypt with incorrect private key should fail', async () => {
-      const encrypted = await cryptoService.encrypt(originalText, keyPair.publicKey);
-      const newKeyPair = await cryptoService.generateKeyPair();
-      expect(cryptoService.decrypt(encrypted, newKeyPair.privateKey)).rejects.toThrow();
-    });
-  });
-
-  describe('integration with AsymmetricEncryptionKeyPair', () => {
-    const originalText = 'Cela devrait fonctionner avec les caractères unicode 😃';
-    let keyPair: AsymmetricEncryptionKeyPair;
-
-    beforeEach(async () => {
-      keyPair = await cryptoService.generateKeyPair();
-    });
-
-    it('should be able to encrypt data, export the key, import the key, and decode the data', async () => {
-      const encrypted = await cryptoService.encrypt(originalText, keyPair.publicKey);
-      const rawKeyPair = await keyPair.export();
-      const importedKeyPair = await AsymmetricEncryptionKeyPair.fromRaw(rawKeyPair);
-      const decrypted = await cryptoService.decrypt(encrypted, importedKeyPair.privateKey);
-      expect(decrypted).toBe(originalText);
     });
   });
 });
