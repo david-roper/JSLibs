66578fda10a295e641bf4571b5e303ccd0e57458
joshunrau <contact@joshuaunrau.com>
2024-01-30 10:22:23 -0500

    add RawAsymmetricEncryptionKeyPair and SerializableUint8Array

--- packages/nestjs/src/modules/crypto/crypto.utils.ts
+++ packages/nestjs/src/modules/crypto/crypto.utils.ts
@@ -1,19 +1,20 @@
-function arrayBufferToString(buffer: ArrayBuffer): string {
-  return String.fromCharCode.apply(null, Array.from(new Uint8Array(buffer)));
-}
-
-function stringToArrayBuffer(str: string): ArrayBuffer {
-  const buf = new ArrayBuffer(str.length);
-  const bufView = new Uint8Array(buf);
-  for (let i = 0, strLen = str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
+export class SerializableUint8Array extends Uint8Array {
+  constructor(array: ArrayBufferLike | ArrayLike<number>) {
+    super(array);
   }
-  return buf;
+
+  toArray() {
+    return Array.from(this);
+  }
+
+  toJSON() {
+    return this.toArray();
+  }
 }
 
-export type ExportedAsymmetricEncryptionKeyPair = {
-  privateKey: string;
-  publicKey: string;
+export type RawAsymmetricEncryptionKeyPair = {
+  privateKey: SerializableUint8Array;
+  publicKey: SerializableUint8Array;
 };
 
 export class AsymmetricEncryptionKeyPair {
@@ -27,23 +28,22 @@ export class AsymmetricEncryptionKeyPair {
     publicExponent: new Uint8Array([1, 0, 1])
   };
 
-  private static privateKeyPemFooter = '-----END PRIVATE KEY-----';
-  private static privateKeyPemHeader = '-----BEGIN PRIVATE KEY-----';
-  private static publicKeyPemFooter = '-----END PUBLIC KEY-----';
-  private static publicKeyPemHeader = '-----BEGIN PUBLIC KEY-----';
-
   constructor({ privateKey, publicKey }: { privateKey: CryptoKey; publicKey: CryptoKey }) {
     this.privateKey = privateKey;
     this.publicKey = publicKey;
   }
 
-  static async fromRaw({ privateKey }: { privateKey: string; publicKey: string }) {
-    return {
-      privateKey: await this.importPrivateKey(privateKey)
-    };
+  static async fromRaw({
+    privateKey,
+    publicKey
+  }: RawAsymmetricEncryptionKeyPair): Promise<AsymmetricEncryptionKeyPair> {
+    return new this({
+      privateKey: await this.importPrivateKey(privateKey),
+      publicKey: await this.importPublicKey(publicKey)
+    });
   }
 
-  static async generate() {
+  static async generate(): Promise<AsymmetricEncryptionKeyPair> {
     const { privateKey, publicKey } = await globalThis.crypto.subtle.generateKey(this.algorithm, true, [
       'encrypt',
       'decrypt'
@@ -51,14 +51,15 @@ export class AsymmetricEncryptionKeyPair {
     return new this({ privateKey, publicKey });
   }
 
-  private static async importPrivateKey(pem: string) {
-    const pemContents = pem.substring(this.privateKeyPemHeader.length, pem.length - this.privateKeyPemFooter.length);
-    const decoded = atob(pemContents);
-    const buffer = stringToArrayBuffer(decoded);
-    return globalThis.crypto.subtle.importKey('pkcs8', buffer, this.algorithm, true, ['decrypt']);
+  private static async importPrivateKey(rawKey: Uint8Array): Promise<CryptoKey> {
+    return globalThis.crypto.subtle.importKey('pkcs8', rawKey, this.algorithm, true, ['decrypt']);
   }
 
-  async export(): Promise<{ privateKey: string; publicKey: string }> {
+  private static async importPublicKey(rawKey: Uint8Array): Promise<CryptoKey> {
+    return globalThis.crypto.subtle.importKey('spki', rawKey, this.algorithm, true, ['encrypt']);
+  }
+
+  async export(): Promise<RawAsymmetricEncryptionKeyPair> {
     return {
       privateKey: await this.exportPrivateKey(this.privateKey),
       publicKey: await this.exportPublicKey(this.publicKey)
@@ -66,38 +67,10 @@ export class AsymmetricEncryptionKeyPair {
   }
 
   private async exportPrivateKey(key: CryptoKey) {
-    const exported = await globalThis.crypto.subtle.exportKey('pkcs8', key);
-    const exportedAsString = arrayBufferToString(exported);
-    const exportedAsBase64 = btoa(exportedAsString);
-    return [this.static.privateKeyPemHeader, exportedAsBase64, this.static.privateKeyPemFooter].join('\n');
+    return new SerializableUint8Array(await globalThis.crypto.subtle.exportKey('pkcs8', key));
   }
 
   private async exportPublicKey(key: CryptoKey) {
-    const exported = await globalThis.crypto.subtle.exportKey('spki', key);
-    const exportedAsString = arrayBufferToString(exported);
-    const exportedAsBase64 = btoa(exportedAsString);
-    return [this.static.publicKeyPemHeader, exportedAsBase64, this.static.publicKeyPemFooter].join('\n');
-  }
-
-  private get static() {
-    return AsymmetricEncryptionKeyPair;
-  }
-
-  // private toPem(content: string, label: string) {
-  //   return `-----BEGIN ${label}-----\n${content}\n-----END PUBLIC KEY-----`;
-  // }
-}
-
-export class EncryptedData extends Uint8Array {
-  constructor(buffer: ArrayBufferLike) {
-    super(buffer);
-  }
-
-  toArray() {
-    return Array.from(this);
-  }
-
-  toJSON() {
-    return this.toArray();
+    return new SerializableUint8Array(await globalThis.crypto.subtle.exportKey('spki', key));
   }
 }
