56cf20465472cac251edb34c829b4bcce1f0fae6
joshunrau <contact@joshuaunrau.com>
2024-02-05 10:07:08 -0500

    @douglasneuroinformatics/ui@5.3.5

--- packages/ui/src/components/PopoverIcon/PopoverIcon.tsx
+++ packages/ui/src/components/PopoverIcon/PopoverIcon.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 
 import { Popover, Transition } from '@headlessui/react';
-import clsx from 'clsx';
+
+import { cn } from '../../utils/cn';
 
 export type PopoverIconProps = {
   icon: React.ComponentType<Omit<React.SVGProps<SVGSVGElement>, 'ref'>>;
+  iconClassName?: string;
   position: 'left' | 'right';
   text: string;
 };
 
-export const PopoverIcon = ({ icon, position, text }: PopoverIconProps) => {
+export const PopoverIcon = ({ icon, iconClassName, position, text }: PopoverIconProps) => {
   // TypeScript wants components to start with capital letter
   const Icon = icon;
   return (
     <Popover className="relative">
       <Popover.Button className="flex items-center justify-center" tabIndex={-1}>
-        <Icon className="text-slate-600 dark:text-slate-300" height={16} width={16} />
+        <Icon className={cn('text-slate-600 dark:text-slate-300 h-4 w-4', iconClassName)} />
       </Popover.Button>
       <Transition
         as={React.Fragment}
@@ -27,7 +29,7 @@ export const PopoverIcon = ({ icon, position, text }: PopoverIconProps) => {
         leaveTo="opacity-0 translate-y-1"
       >
         <Popover.Panel
-          className={clsx(
+          className={cn(
             'absolute z-10 mt-2 w-max max-w-xs rounded-md border border-slate-300 bg-slate-100 p-2 text-sm text-slate-600 shadow-sm dark:border-slate-600 dark:bg-slate-800 dark:text-slate-300',
             {
               'left-0': position === 'left',
