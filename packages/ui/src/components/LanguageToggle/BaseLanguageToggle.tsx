e80024a262c774e41ab108413549d04240ea7707
joshunrau <contact@joshuaunrau.com>
2024-02-21 12:10:34 -0500

    adjust z-index

--- packages/ui/src/components/LanguageToggle/BaseLanguageToggle.tsx
+++ packages/ui/src/components/LanguageToggle/BaseLanguageToggle.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/24/solid';
-import { clsx } from 'clsx';
 import type { Promisable } from 'type-fest';
 
+import { cn } from '../../utils/cn';
+
 export type BaseLanguageToggleProps<T extends string> = {
+  className?: string;
   dropdownDirection?: 'down' | 'up';
   i18n: {
     changeLanguage: (lang: T) => Promisable<unknown>;
@@ -15,22 +17,19 @@ export type BaseLanguageToggleProps<T extends string> = {
 };
 
 export const BaseLanguageToggle = <T extends string = string>({
+  className,
   dropdownDirection,
   i18n,
   options
 }: BaseLanguageToggleProps<T>) => {
   return (
-    <Menu as="div" className="relative bg-inherit">
+    <Menu as="div" className={cn('relative bg-inherit', className)}>
       <Menu.Button
         className="flex items-center justify-center rounded-md p-2 hover:backdrop-brightness-95 dark:hover:backdrop-brightness-150"
         type="button"
       >
         <span className="uppercase">{i18n.resolvedLanguage}</span>
-        <ChevronDownIcon
-          className={clsx('ml-1', { 'rotate-180': dropdownDirection === 'up' })}
-          height={16}
-          width={16}
-        />
+        <ChevronDownIcon className={cn('ml-1', { 'rotate-180': dropdownDirection === 'up' })} height={16} width={16} />
       </Menu.Button>
       <Transition
         as={React.Fragment}
@@ -42,8 +41,8 @@ export const BaseLanguageToggle = <T extends string = string>({
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items
-          className={clsx(
-            'absolute flex min-w-full flex-col bg-inherit bg-opacity-100 shadow-sm rounded-md ring-1 ring-slate-900/5 dark:ring-slate-100/20',
+          className={cn(
+            'absolute flex min-w-full z-30 flex-col bg-inherit bg-opacity-100 shadow-sm rounded-md ring-1 ring-slate-900/5 dark:ring-slate-100/20',
             {
               'bottom-12': dropdownDirection === 'up',
               'right-0 mt-2 origin-right': dropdownDirection !== 'up'
