805d06fcc388bb100a31216ffee9b041197be624
joshunrau <contact@joshuaunrau.com>
2024-02-28 12:37:22 -0500

    use tsup for build

---
+++ packages/ui/tsup.config.js
@@ -0,0 +1,19 @@
+import path from 'path';
+import url from 'url';
+
+import { defineConfig } from 'tsup';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: [path.resolve(__dirname, 'src/index.ts')],
+  format: 'esm',
+  outDir: path.resolve(__dirname, 'dist'),
+  platform: 'browser',
+  sourcemap: true,
+  splitting: true,
+  target: 'esnext'
+});
